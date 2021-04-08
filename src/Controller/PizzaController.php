124274f9d514fac42016bc7f77eab2be1b2da56d
David Jegat <david.jegat@knplabs.com>
2021-04-08 17:43:14 +0200

    Use symfony good practice

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -3,23 +3,25 @@
 namespace App\Controller;
 
 use App\Repository\PizzaRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
-class PizzaController extends AbstractController
+class PizzaController
 {
     /**
      * List l'intégralité des pizzas disponible
      * 
      * @Route("/pizzas", name="list_pizzas", methods={"GET"})
      */
-    public function listPizzas(PizzaRepository $repository): Response
+    public function listPizzas(PizzaRepository $repository, Environment $twig): Response
     {
         $pizzas = $repository->findAll();
 
-        return $this->render('pizza/list.html.twig', [
+        $template = $twig->render('pizza/list.html.twig', [
             'pizzas' => $pizzas,
         ]);
+
+        return new Response($template);
     }
 }
