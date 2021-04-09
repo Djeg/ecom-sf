11f2f75c343c4f3e8ae27e161c397453bd417b45
David Jegat <david.jegat@knplabs.com>
2021-04-09 11:32:09 +0200

    Clean up controller and template

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -6,11 +6,7 @@ namespace App\Controller;
 
 use App\Entity\Pizza;
 use App\Form\PizzaType;
-use Twig\Environment;
-use App\Repository\PizzaRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,51 +16,46 @@ class PizzaController extends AbstractController
     /**
      * List l'intégralité des pizzas disponible
      * 
-     * @Route("/pizzas", name="list_pizzas", methods={"GET"})
+     * @Route("/pizzas", name="pizza_list", methods={"GET"})
      */
-    public function listPizzas(PizzaRepository $repository, Environment $twig): Response
+    public function list(): Response
     {
-        $pizzas = $repository->findAll();
+        $pizzas = $this
+            ->getDoctrine()
+            ->getRepository(Pizza::class)
+            ->findAll();
 
-        $template = $twig->render('pizza/list.html.twig', [
+        return $this->render('pizza/list.html.twig', [
             'pizzas' => $pizzas,
         ]);
-
-        return new Response($template);
     }
 
     /**
      * Créé une nouvelle pizza en utilisant le composant de formulaire
      * *
-     * @Route("/pizzas/new", name="create_pizza", methods={"GET", "POST"})
+     * @Route("/pizzas/new", name="pizza_create", methods={"GET", "POST"})
      */
-    public function createPizza(
-        EntityManagerInterface $manager,
-        FormFactoryInterface $formFactory,
-        Environment $twig,
-        Request $request
-    ): Response {
+    public function create(Request $request): Response
+    {
         $pizza = new Pizza();
 
-        $form = $formFactory->create(PizzaType::class, $pizza);
+        $form = $this->createForm(PizzaType::class, $pizza);
         $form->handleRequest($request);
-        $isSuccess = false;
 
         if ($form->isSubmitted() && $form->isValid()) {
             $task = $form->getData();
 
+            $manager = $this->getDoctrine()->getManager();
+
             $manager->persist($task);
             $manager->flush();
 
-            $isSuccess = true;
+            return $this->redirectToRoute('pizza_list');
         }
 
-        $template = $twig->render('pizza/create.html.twig', [
+        return $this->render('pizza/create.html.twig', [
             'form' => $form->createView(),
-            'isSuccess' => $isSuccess,
             'pizza' => $pizza,
         ]);
-
-        return new Response($template);
     }
 }
